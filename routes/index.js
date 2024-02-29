6b12e71cd5e5414b6f7b084d934a42e6fd52d117
Harsh veer singh <harshveer7828@gmail.com>
2024-02-29 23:06:14 +0530

    cart ejs added

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/home', function(req, res, next) {
+  res.render('home');
 });
+router.get('/cart', function(req, res, next) {
+  res.render('cart');
+});
+router.get('/login', function(req, res, next) {
+  res.render('auth/login');
+});
+router.get('/cart', function(req, res, next) {
+  res.render('cart');
+});
+
+
 
 module.exports = router;
